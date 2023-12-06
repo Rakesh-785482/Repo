6c07cd26dc1fc3cf5b9f232f64ed9ffd3a7319ff
Rakesh-785482 <rakeshkumaharana5@gmail.com>
2023-12-06 19:20:58 +0530

    This is For Second Message

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("Hii Rakesh");
         System.out.println(new Date());
+        System.out.println("This Is My Second Message...");
     }
 }
