3966a7abf3090c043e2843893813d4a5df2a3a62
Rakesh-785482 <rakeshkumaharana5@gmail.com>
2023-12-06 18:54:12 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hii Rakesh");
+        System.out.println(new Date());
+    }
+}
